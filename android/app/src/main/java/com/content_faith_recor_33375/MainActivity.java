3f6ff2b7833f282d1136d477c6a533591dd5fb63
Crowdbotics <team@crowdbotics.com>
2022-02-05 02:47:50 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/content_faith_recor_33375/MainActivity.java
@@ -0,0 +1,15 @@
+package com.content_faith_recor_33375;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "content_faith_recor_33375";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "content_faith_recor_33375",
+  url: "https://content_faith_recor_33375.botics.co",
+  api: "https://content_faith_recor_33375.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
